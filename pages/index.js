e5ff01e2b6d25f4c56b26f064c4e046a3003a78f
Ryan Milligan <90968645+rmilligan006@users.noreply.github.com>
2022-06-06 14:05:10 -0400

    bug changes

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Head from "next/head";
 import Main from "../components/Main";
 
 import About from "../components/About";
-import Skills from "../components/skills";
+import Skills from "../components/Skills";
 import Projects from "../components/Projects";
 import Contact from "../components/Contact";
 
